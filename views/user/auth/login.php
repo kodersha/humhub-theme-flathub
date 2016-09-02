c08aa765bed48a70a0cb77f3c34056d59ece4337
Srijith <srijith@qburst.com>
2016-09-02 11:32:02 +0530

    Fixed issue on login page which reference invalid registermodel. Replaced with invite model instead

--- views/user/auth/login.php
+++ views/user/auth/login.php
@@ -55,9 +55,9 @@ $this->pageTitle = Yii::t('UserModule.views_auth_login', 'Login');
 
                 <p><?php echo Yii::t('UserModule.views_auth_login', "Don't have an account? Join the network by entering your e-mail address."); ?></p>
 
-                <?php $form = ActiveForm::begin(['id' => 'account-register-form']); ?>
+                <?php $form = ActiveForm::begin(['id' => 'invite-form']); ?>
 
-                <?php echo $form->field($registerModel, 'email')->textInput(['id' => 'register-email', 'placeholder' => $registerModel->getAttributeLabel('email')])->label(false); ?>
+                <?php echo $form->field($invite, 'email')->textInput(['id' => 'register-email', 'placeholder' => $invite->getAttributeLabel('email')])->label(false); ?>
                 <hr>
                 <?php echo CHtml::submitButton(Yii::t('UserModule.views_auth_login', 'Register'), array('class' => 'btn btn-primary')); ?>
 
@@ -84,7 +84,7 @@ $this->pageTitle = Yii::t('UserModule.views_auth_login', 'Login');
 <?php } ?>
 
     // Shake panel after wrong validation
-<?php if ($registerModel->hasErrors()) { ?>
+<?php if ($invite->hasErrors()) { ?>
         $('#register-form').removeClass('bounceInLeft');
         $('#register-form').addClass('shake');
         $('#login-form').removeClass('bounceIn');
