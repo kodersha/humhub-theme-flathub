77cd4c4e2fdac716fd230ecad6ae016751bb2b59
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-04-03 04:43:44 -0400

    Update _layout.php

--- views/user/profile/_layout.php
+++ views/user/profile/_layout.php
@@ -4,7 +4,7 @@ $user = $this->context->getUser();
 <div class="container profile-layout-container">
     <div class="row"> 
         <div class="col-md-12">
-            <?php echo \humhub\modules\user\widgets\ProfileHeader::widget(['user' => $user]); ?>
+            <?= \humhub\modules\user\widgets\ProfileHeader::widget(['user' => $user]); ?>
         </div>
     </div>
     <div class="row">
@@ -16,12 +16,12 @@ $user = $this->context->getUser();
     : ?>
 			<div class="col-md-4 layout-sidebar-container">
 				<div class="panel-light">
-					<?php echo \humhub\modules\user\widgets\ProfileMenu::widget(['user' => $this->context->user]);
+					<?= \humhub\modules\user\widgets\ProfileMenu::widget(['user' => $this->context->user]);
 }
 ?>
                 </div>
-				<?php
-                echo \humhub\modules\user\widgets\ProfileSidebar::widget([
+				<?=
+	\humhub\modules\user\widgets\ProfileSidebar::widget([
                     'user' => $this->context->user,
                     'widgets' => [
                         [\humhub\modules\user\widgets\UserTags::className(), ['user' => $this->context->user], ['sortOrder' => 10]],
@@ -32,7 +32,7 @@ $user = $this->context->getUser();
                 ?>
             </div>
             <div class="col-md-8 panel-hidden layout-content-container">
-                <?php echo $content; ?>
+                <?= $content; ?>
             </div>
         <?php endif; ?>
     </div>
