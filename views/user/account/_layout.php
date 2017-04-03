fda92968c56e2e2763e4e664375c32e87d23149c
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-04-03 05:08:27 -0400

    Update _layout.php

--- views/user/account/_layout.php
+++ views/user/account/_layout.php
@@ -3,18 +3,18 @@ $user = $this->context->getUser();
 ?>
 <div class="container">
     <div class="row">
-        <div class="col-md-12">
+        <div class="col-md-3">
             <?= \humhub\modules\user\widgets\ProfileHeader::widget(['user' => $user]); ?>
         </div>
     </div>
     <div class="row">
         <div class="col-md-4 panel-light no-icons">
 			<?= \humhub\modules\user\widgets\ProfileMenu::widget(['user' => $this->context->user]); ?>
-            <?php echo \humhub\modules\user\widgets\AccountMenu::widget(); ?>
+            <?= \humhub\modules\user\widgets\AccountMenu::widget(); ?>
         </div> 
-        <div class="col-md-8 panel-hidden">
+        <div class="col-md-9 panel-hidden">
             <div class="panel panel-default">
-                <?php echo $content; ?>
+                <?= $content; ?>
             </div>
         </div>
     </div>
