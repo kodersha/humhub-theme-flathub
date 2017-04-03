d179a4ea82326fed0982e1071d0c02480570d339
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-04-03 05:51:45 -0400

    Update logo.php

--- views/widgets/logo.php
+++ views/widgets/logo.php
@@ -4,27 +4,26 @@ use yii\helpers\Html;
 ?>
 <?php if ($place == "topMenu") : ?>
     <?php if ($logo->hasImage()) : ?>
-        <a class="navbar-brand image hidden-xs" href="<?= Yii::$app->homeUrl; ?>">
-            <img src="<?= $logo->getUrl(); ?>" id="img-logo"/>
+        <a class="navbar-brand hidden-xs" style="height: 50px; padding:5px;"
+           href="<?= Yii::$app->homeUrl; ?>">
+            <img class="img-rounded" src="<?= $logo->getUrl(); ?>"
+                 id="img-logo"/>
         </a>
     <?php endif; ?>
     <a class="navbar-brand" style="<?php if ($logo->hasImage()) : ?>display:none;<?php endif; ?> "
-       href="<?php echo Yii::$app->homeUrl; ?>" id="text-logo">
-			<!-- <img src="<?php echo $this->theme->getBaseUrl().'/img/logo.png'; ?>"></img> -->
-			<?= Html::encode(Yii::$app->name); ?>
+       href="<?= Yii::$app->homeUrl; ?>" id="text-logo">
+           <?= Html::encode(Yii::$app->name); ?>
     </a>
 <?php endif; ?>
 
 <?php if ($place == "login") : ?>
     <?php if ($logo->hasImage()) : ?>
-        <a class="login-logo" href="<?= Yii::$app->homeUrl; ?>">
-            <img src="<?= $logo->getUrl(); ?>" id="img-logo"/>
+        <a href="<?= Yii::$app->homeUrl; ?>">
+            <img class="img-rounded" src="<?= $logo->getUrl(); ?>"
+                 id="img-logo"/>
         </a>
         <br>
-    <?php else {
-    : ?>
-        <h1 id="app-title" class="animated fadeIn"><a href="<?= Yii::$app->homeUrl;
-}
-?>"><?= Html::encode(Yii::$app->name); ?></a></h1>
+    <?php else: ?>
+        <h1 id="app-title" class="animated fadeIn"><?= Html::encode(Yii::$app->name); ?></h1>
     <?php endif; ?>
 <?php endif; ?>
