a54e0d6c9b809ad16f7693c9c95df6c44e269ccf
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-04-02 20:38:34 -0400

    Update main.php

--- views/admin/layouts/main.php
+++ views/admin/layouts/main.php
@@ -1,10 +1,10 @@
 <div class="container">
     <div class="row">
-        <div class="col-md-4 no-icons">
+        <div class="col-md-3">
             <?= \humhub\modules\admin\widgets\AdminMenu::widget(); ?>
         </div>
-        <div class="col-md-8">
-            <?php echo $content; ?> 
+        <div class="col-md-9">
+            <?= $content; ?>
         </div>
     </div>
 </div>
