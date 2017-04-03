fa0760e681ba0d09537492a5caf940b70982529f
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-04-03 05:29:31 -0400

    Update shareLink.php

--- views/socialshare/widgets/shareLink.php
+++ views/socialshare/widgets/shareLink.php
@@ -1,4 +1,5 @@
 <?php
+
 use yii\helpers\Html;
 ?>
 
@@ -25,9 +26,9 @@ use yii\helpers\Html;
 
 			";
         ?>
-		<?php echo Html::a('<i class="fa fa-facebook" style="font-size:16px;color:#3a5795">&nbsp;</i>', 'https://www.facebook.com/sharer/sharer.php?u=' . urlencode($permalink) . '&description=' . urlencode($object->getContentDescription()),['onclick'=> $option]);?>
-		<?php echo Html::a('<i class="fa fa-twitter" style="font-size:16px;color:#55acee">&nbsp;</i>&nbsp;</i>', 'https://twitter.com/intent/tweet?text=' . urlencode($object->getContentDescription()) . '&url=' . urlencode($permalink),['onclick'=> $option]);?>
-		<?php echo Html::a('<i class="fa fa-linkedin-square" style="font-size:16px;color:#0177b5"></i>', 'https://www.linkedin.com/shareArticle?summary=&mini=true&source=&title=' . urlencode($object->getContentDescription()) . '&url=' . urlencode($permalink) . '&ro=false', ['onclick'=> $option]);?>
+		<?= Html::a('<i class="fa fa-facebook" style="font-size:16px;color:#3a5795">&nbsp;</i>', 'https://www.facebook.com/sharer/sharer.php?u=' . urlencode($permalink) . '&description=' . urlencode($object->getContentDescription()),['onclick'=> $option]);?>
+		<?= Html::a('<i class="fa fa-twitter" style="font-size:16px;color:#55acee">&nbsp;</i>&nbsp;</i>', 'https://twitter.com/intent/tweet?text=' . urlencode($object->getContentDescription()) . '&url=' . urlencode($permalink),['onclick'=> $option]);?>
+		<?= Html::a('<i class="fa fa-linkedin-square" style="font-size:16px;color:#0177b5"></i>', 'https://www.linkedin.com/shareArticle?summary=&mini=true&source=&title=' . urlencode($object->getContentDescription()) . '&url=' . urlencode($permalink) . '&ro=false', ['onclick'=> $option]);?>
 	</div>
   </div>
 </div>
